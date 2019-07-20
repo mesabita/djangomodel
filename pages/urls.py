8876c03c21a9656dfa3d9615e4c836a738901ff3
Sabita Regmi Sharma <aavaregmi365@gmail.com>
2019-07-20 11:00:09 +0530

    url name changed

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,6 @@
 from django .urls import path
 from . import views
 urlpatterns = [
-    path('',views.HomePageView.as_view(),name='home'),
+    path('',views.HomePageView.as_view(),name='home1'),
     path('about/',views.AboutPageView.as_view(),name='about'),  
 ]
